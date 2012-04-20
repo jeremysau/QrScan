1d8ac758a7a1b019bff3ce46b6535916acb6545d
Jeremy Sau <jeremy.sau@gmail.com>
2012-04-20 09:34:15 +0200

    Initial Commit

---
+++ QrScan/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  QrScan
+//
+//  Created by lionel Faverie on 20/04/12.
+//  Copyright (c) 2012 Mobibase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ QrScan/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QrScan
+//
+//  Created by lionel Faverie on 20/04/12.
+//  Copyright (c) 2012 Mobibase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
